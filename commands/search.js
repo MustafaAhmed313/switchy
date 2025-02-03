92cc601ebc241a2cc4f2e5878543af15ff0f316b
zoombie <abdelrhmansayed122003@gmail.com>
2025-02-03 16:31:00 +0200

    write docs for search command

--- commands/search.js
+++ commands/search.js
@@ -2,16 +2,42 @@ const { FileOperator } = require("../utils/fileOperator");
 const { JsonOperator } = require("../utils/jsonOperator");
 const { RepoOperator } = require("../utils/repoOperator");
 const { getDataPath } = require("../utils/pathModule");
+
+/**
+ * Searches for a repository by name in the data file.
+ *
+ * This function:
+ * - Retrieves the path of the data file using `getDataPath()`.
+ * - Reads the data file using `FileOperator.readFromFile()`.
+ * - Parses the JSON data using `JsonOperator.parsingJsonData()`.
+ * - Searches for the repository index using `RepoOperator.getRepoIndexByName()`.
+ *
+ * @param {string} name - The name of the repository to search for.
+ * @returns {Object|string} - Returns the repository object if found, otherwise returns `"NotExist"`.
+ */
 const search = (name) => {
+  // Get the path to the data file
   const path = getDataPath();
+
+  // Read the data file content
   let data = FileOperator.readFromFile(path);
+
+  // Parse the file content into a JSON object
   data = JsonOperator.parsingJsonData(data);
+
+  // Find the index of the repository with the given name
   const repoIndex = RepoOperator.getRepoIndexByName(data["repositories"], name);
+
+  // If the repository is not found, return "NotExist"
   if (repoIndex === -1) {
     return "NotExist";
   }
+
+  // Return the found repository object
   return data["repositories"][repoIndex];
 };
+
+// Export the search function for use in other modules
 module.exports = {
   search,
 };
