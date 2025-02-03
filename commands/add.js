41884867207b75eb3db874a6f2c6a9b1da0f5418
zoombie <abdelrhmansayed122003@gmail.com>
2025-02-03 11:14:47 +0200

    Replace .env file with config.js file

--- commands/add.js
+++ commands/add.js
@@ -1,60 +1,65 @@
-const os = require('os'); 
+const os = require("os");
 
 const { Repository } = require("../models/repository");
-const { FileOperator } = require('../utils/fileOperator');
-const { JsonOperator } = require('../utils/jsonOperator');
-const { Log } = require('../models/log');
-const { 
-  logger, 
-  STATUS 
-} = require('../utils/logger');
-const { 
-  getSuccessMessage, 
-  TYPES, 
-  getErrorMessage
-} = require('../utils/messageHandler');
+const { FileOperator } = require("../utils/fileOperator");
+const { JsonOperator } = require("../utils/jsonOperator");
+const { Log } = require("../models/log");
+const { logger, STATUS } = require("../utils/logger");
+
+const config = require("../config/config");
+const {
+  getSuccessMessage,
+  TYPES,
+  getErrorMessage,
+} = require("../utils/messageHandler");
 
 const currentOS = os.type();
 
 const add = (path) => {
   if (!path) {
-    return logger(new Log(
-      STATUS.FAILED,
-      getErrorMessage(TYPES.REQUIRED, 'Repository path is')
-    ));
+    return logger(
+      new Log(
+        STATUS.FAILED,
+        getErrorMessage(TYPES.REQUIRED, "Repository path is")
+      )
+    );
   }
 
   const condition = currentOS === `Linux` || currentOS === `Darwin`;
-  const index = path.lastIndexOf(condition ? '/' :`\\`) + 1;
+  const index = path.lastIndexOf(condition ? "/" : `\\`) + 1;
   const name = path.substring(index);
-  
+
   const repository = new Repository(
-    name, path, new Date(Date.now()).toUTCString()
+    name,
+    path,
+    new Date(Date.now()).toUTCString()
   );
 
   const data = FileOperator.readFromFile(
-    `${process.env.DIRECTORY_DATA}/${process.env.REPOSITORY_NAME}.json` 
+    `${config.DIRECTORY_DATA}/${config.REPOSITORY_NAME}.json`
   );
 
   if (!data) {
-    return logger(new Log(STATUS.FAILED, getErrorMessage(TYPES.INIT))); 
+    return logger(new Log(STATUS.FAILED, getErrorMessage(TYPES.INIT)));
   }
 
   const parsedData = JsonOperator.parsingJsonData(data);
   parsedData["repositories"].push(repository);
 
   FileOperator.writeToFile(
-    `${process.env.DIRECTORY_DATA}/${process.env.REPOSITORY_NAME}.json`,
-    JSON.stringify(parsedData) 
+    `${config.DIRECTORY_DATA}/${config.REPOSITORY_NAME}.json`,
+    JSON.stringify(parsedData)
   );
 
-  logger(new Log(
-    STATUS.SUCCESS,
-    getSuccessMessage(TYPES.ADD, 'repository'),
-    repository
-  ));
-}
+  logger(
+    new Log(
+      STATUS.SUCCESS,
+      getSuccessMessage(TYPES.ADD, "repository"),
+      repository
+    )
+  );
+};
 
 module.exports = {
-  add
-}
+  add,
+};
