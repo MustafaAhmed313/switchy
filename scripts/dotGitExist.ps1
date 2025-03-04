ff3cd297d8719d0ae1b4387b1125d02aaa979d74
zoombie <abdelrhmansayed122003@gmail.com>
2025-03-04 16:44:50 +0200

    refactored logic related to directory and file creation

--- scripts/dotGitExist.ps1
+++
@@ -1,8 +0,0 @@
-param([string]$dirPath)
-
-# Check if the .git directory exists
-if (Test-Path "$dirPath\.git") {
-    Write-Output "true"
-} else {
-    Write-Output "false"
-}
--- utils/pathModule.js
+++ utils/pathModule.js
@@ -3,28 +3,27 @@ const os = require("os");
 
 const config = require("../config/config");
 
-exports.getDataPath = function () {
+const getDataPath = function () {
   return path.join(
     __dirname,
     `../${config.DIRECTORY_DATA}/${config.REPOSITORY_NAME}.json`
   );
 };
 
-exports.getDirPath = function () {
-  return path.join(
-    __dirname,
-    `../${config.DIRECTORY_DATA}`
-  );
-};
-
-exports.getPath = function (filePath) {
+const getPath = function (filePath) {
   return path.join(__dirname, filePath);
 };
 
-exports.getName = function (path) {
+const getName = function (path) {
   const currentOS = os.type();
   const condition = currentOS === `Linux` || currentOS === `Darwin`;
   const index = path.lastIndexOf(condition ? "/" : `\\`) + 1;
   const name = path.substring(index);
   return name;
+};
+
+module.exports = {
+  getDataPath,
+  getPath,
+  getName,
 };
